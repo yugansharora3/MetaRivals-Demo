4c8b41ed312d1137050a07675e8712c8c9a66dce
yugansharora01 <yugansharora01@gmail.com>
2022-12-08 18:11:17 +0530

    fixed swipe issue

--- Assets/scripts/FixedTouchField.cs
+++ Assets/scripts/FixedTouchField.cs
@@ -1,8 +1,11 @@
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class FixedTouchField : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    public float lookSpeed = 2.0f, XSenstivity = 1.0f,YSenstivity = 0.01f;
+    public bool OnPc = false;
     [HideInInspector]
     public Vector2 TouchDist;
     [HideInInspector]
@@ -11,16 +14,23 @@ public class FixedTouchField : MonoBehaviour, IPointerDownHandler, IPointerUpHan
     protected int PointerId;
     [HideInInspector]
     public bool Pressed;
+    public CinemachineFreeLook Cinemachine;
 
     // Use this for initialization
     void Start()
     {
-
+        //cinemachine = Cinemachine.GetComponent<CinemachineFreeLook>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(OnPc)
+        {
+            XSenstivity = 10;
+            YSenstivity = 0.1f;
+        }
         if (Pressed)
         {
             if (PointerId >= 0 && PointerId < Input.touches.Length)
@@ -38,6 +48,8 @@ public class FixedTouchField : MonoBehaviour, IPointerDownHandler, IPointerUpHan
         {
             TouchDist = new Vector2();
         }
+        Cinemachine.m_XAxis.Value += TouchDist.x * Time.deltaTime * lookSpeed * XSenstivity;
+        Cinemachine.m_YAxis.Value += TouchDist.y * Time.deltaTime * lookSpeed * YSenstivity;
     }
 
     public void OnPointerDown(PointerEventData eventData)
--- Assets/scripts/Swipe.cs
+++ Assets/scripts/Swipe.cs
@@ -38,104 +38,104 @@ public class Swipe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        tap = swipeLeft = swipeRight = swipeUp = swipeDown = false;
+        //tap = swipeLeft = swipeRight = swipeUp = swipeDown = false;
 
-        int lastTouchIndex = Input.touches.Length - 1;
+        //int lastTouchIndex = Input.touches.Length - 1;
 
-        #region Standalone Inputs
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (check())
-            {
-                tap = true;
-                isDragging = true;
-                startTouch = Input.mousePosition;
-            }
+        //#region Standalone Inputs
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    if (check())
+        //    {
+        //        tap = true;
+        //        isDragging = true;
+        //        startTouch = Input.mousePosition;
+        //    }
                 
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            isDragging = false;
-            Reset();
-        }
-        #endregion
+        //}
+        //else if (Input.GetMouseButtonUp(0))
+        //{
+        //    isDragging = false;
+        //    Reset();
+        //}
+        //#endregion
 
-        #region Mobile Inputs
-        if (Input.touches.Length > 0)
-        {
-            if (Input.touches[lastTouchIndex].phase == TouchPhase.Began)
-            {
-                if(!CheckIfPointIsOnUI(Input.touches[lastTouchIndex].position))
-                {
-                    if(check())
-                    {
-                        tap = true;
-                        isDragging = true;
-                        startTouch = Input.touches[lastTouchIndex].position;
-                    }
+        //#region Mobile Inputs
+        //if (Input.touches.Length > 0)
+        //{
+        //    if (Input.touches[lastTouchIndex].phase == TouchPhase.Began)
+        //    {
+        //        if(!CheckIfPointIsOnUI(Input.touches[lastTouchIndex].position))
+        //        {
+        //            if(check())
+        //            {
+        //                tap = true;
+        //                isDragging = true;
+        //                startTouch = Input.touches[lastTouchIndex].position;
+        //            }
                     
-                }
+        //        }
                 
-            }
-            else if (Input.touches[lastTouchIndex].phase == TouchPhase.Ended || Input.touches[lastTouchIndex].phase == TouchPhase.Canceled )
-            {
-                isDragging = false;
-                Reset();
-            }
-        }
-        #endregion
+        //    }
+        //    else if (Input.touches[lastTouchIndex].phase == TouchPhase.Ended || Input.touches[lastTouchIndex].phase == TouchPhase.Canceled )
+        //    {
+        //        isDragging = false;
+        //        Reset();
+        //    }
+        //}
+        //#endregion
         
-        swipeDelta = Vector2.zero;
-        if(isDragging)
-        {
+        //swipeDelta = Vector2.zero;
+        //if(isDragging)
+        //{
 
-            if (Input.touches.Length > 0)
-            {
-                if (!CheckIfPointIsOnUI(Input.touches[lastTouchIndex].position))
-                {
-                    if(check())
-                    {
-                        swipeDelta = Input.touches[lastTouchIndex].position - startTouch;
+        //    if (Input.touches.Length > 0)
+        //    {
+        //        if (!CheckIfPointIsOnUI(Input.touches[lastTouchIndex].position))
+        //        {
+        //            if(check())
+        //            {
+        //                swipeDelta = Input.touches[lastTouchIndex].position - startTouch;
 
-                    }
-                }
-            }
-            else if (Input.GetMouseButton(0))
-            {
-                if (check())
-                    swipeDelta = (Vector2)Input.mousePosition - startTouch;
-            }
+        //            }
+        //        }
+        //    }
+        //    else if (Input.GetMouseButton(0))
+        //    {
+        //        if (check())
+        //            swipeDelta = (Vector2)Input.mousePosition - startTouch;
+        //    }
 
-        }
+        //}
                 
-        t += (5f * Time.deltaTime);
-        float xval = Mathf.Lerp(0f, swipeDelta.x * 1 * lookSpeed * Time.deltaTime, t);
-        cinemachine.m_XAxis.Value += swipeDelta.x * 1 * lookSpeed * Time.deltaTime;
-        float yval = Mathf.Lerp(0f, -swipeDelta.y * 0.01f * lookSpeed * Time.deltaTime, t);
-        cinemachine.m_YAxis.Value += -swipeDelta.y * 0.01f * lookSpeed * Time.deltaTime;
+        //t += (5f * Time.deltaTime);
+        //float xval = Mathf.Lerp(0f, swipeDelta.x * 1 * lookSpeed * Time.deltaTime, t);
+        //cinemachine.m_XAxis.Value += swipeDelta.x * 1 * lookSpeed * Time.deltaTime;
+        //float yval = Mathf.Lerp(0f, -swipeDelta.y * 0.01f * lookSpeed * Time.deltaTime, t);
+        //cinemachine.m_YAxis.Value += -swipeDelta.y * 0.01f * lookSpeed * Time.deltaTime;
         
         
 
-        if (Input.touches.Length > 0)
-        {
-            if (Input.touches[lastTouchIndex].phase == TouchPhase.Moved)
-            {
-                if (!CheckIfPointIsOnUI(Input.touches[lastTouchIndex].position))
-                {
+        //if (Input.touches.Length > 0)
+        //{
+        //    if (Input.touches[lastTouchIndex].phase == TouchPhase.Moved)
+        //    {
+        //        if (!CheckIfPointIsOnUI(Input.touches[lastTouchIndex].position))
+        //        {
                     
-                        tap = true;
-                        isDragging = true;
-                        startTouch = Input.touches[lastTouchIndex].position;
+        //                tap = true;
+        //                isDragging = true;
+        //                startTouch = Input.touches[lastTouchIndex].position;
                     
-                }
-            }
-        }
+        //        }
+        //    }
+        //}
 
-        if (t > 1.0f)
-        {
-            t = 0.0f;
-            storedDelta = Vector2.zero;
-        }
+        //if (t > 1.0f)
+        //{
+        //    t = 0.0f;
+        //    storedDelta = Vector2.zero;
+        //}
     }
 
     private void Reset()
