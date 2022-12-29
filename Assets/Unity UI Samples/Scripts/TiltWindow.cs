49ff6163fbe5179e6393d318cf1e50957543dff3
yugansharora01 <yugansharora01@gmail.com>
2022-12-29 22:56:49 +0530

    StatPanel started

--- Assets/Unity UI Samples/Scripts/TiltWindow.cs
+++ Assets/Unity UI Samples/Scripts/TiltWindow.cs
@@ -13,17 +13,4 @@ public class TiltWindow : MonoBehaviour
 		mTrans = transform;
 		mStart = mTrans.localRotation;
 	}
-
-	void Update ()
-	{
-		//Vector3 pos = Input.mousePosition;
-
-		//float halfWidth = Screen.width * 0.5f;
-		//float halfHeight = Screen.height * 0.5f;
-		//float x = Mathf.Clamp((pos.x - halfWidth) / halfWidth, -1f, 1f);
-		//float y = Mathf.Clamp((pos.y - halfHeight) / halfHeight, -1f, 1f);
-		//mRot = Vector2.Lerp(mRot, new Vector2(x, y), Time.deltaTime * 5f);
-
-		//mTrans.localRotation = mStart * Quaternion.Euler(-mRot.y * range.y, mRot.x * range.x, 0f);
-	}
 }
