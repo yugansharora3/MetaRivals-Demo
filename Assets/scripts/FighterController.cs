fb87be16239b7a973cf972a75979050b417634bd
yugansharora01 <yugansharora01@gmail.com>
2023-01-21 20:52:18 +0530

    Animations work

--- Assets/scripts/FighterController.cs
+++ Assets/scripts/FighterController.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditorInternal.ReorderableList;
 
 public class FighterController : MonoBehaviour
 {
     private Animator anim;
 
     private ShibaControls playerinput;
-    private CharacterController controller;
+    //private CharacterController controller;
 
     private Transform cameraMain;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private bool IsPunching = false;
     private bool IsKicking = false;
+    private bool IsJumping = false;
+    private bool IsIdle = false;
+    private bool IsMoving = false;
     private Vector2 forward;
-    bool CoinCollected = false;
+    bool CanChangeHeight = true;
 
     [SerializeField]
     private float playerSpeed = 25.0f;
@@ -34,8 +38,6 @@ public class FighterController : MonoBehaviour
         forward = transform.forward;
         anim = GetComponent<Animator>();
         playerinput = new ShibaControls();
-        controller = GetComponent<CharacterController>();
-        controller.detectCollisions = true;
     }
 
     private void OnEnable()
@@ -55,15 +57,20 @@ public class FighterController : MonoBehaviour
 
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
+        //groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
         }
 
         Vector2 movementInput = playerinput.PlayerMain.Move.ReadValue<Vector2>();
+
+        if(CanChangeHeight)
+        {
+            anim.SetFloat("height",movementInput.y);
+        }
+
         Vector3 move = forward * movementInput.x;
-        anim.SetFloat("height",movementInput.y);
         move.y = 0f;
         float mag = Vector3.Magnitude(move);
         float moveSpeed;
@@ -72,23 +79,40 @@ public class FighterController : MonoBehaviour
         else
             moveSpeed = playerSpeed;
 
-        transform.position += moveSpeed * Time.deltaTime * move;
+        transform.position += moveSpeed * Time.deltaTime * move * 0.1f;
         //controller.Move(moveSpeed * Time.deltaTime * move);
         anim.SetFloat("moveSpeed", mag);
+        if (movementInput.x == 0 && !IsMoving)
+        {
+            if(!IsIdle)
+            {
+                anim.SetTrigger("IdleTrig");
+                IsIdle = true;
+                
+            }
+        }
+        else
+        {
+            if(!IsMoving)
+                anim.SetTrigger("Move");
+            IsIdle = false;
+        }
 
         if (move != Vector3.zero)
         {
             //anim.SetBool("move", true);
-            gameObject.transform.forward = move;
+            //gameObject.transform.forward = move;
         }
         else
         {
             //anim.SetBool("move", false);
         }
         
-        if (playerinput.PlayerMain.Jump.triggered && groundedPlayer)
+        if (playerinput.PlayerMain.Jump.triggered && !IsJumping)
         {
             anim.SetBool("jump",true);
+            anim.SetTrigger("Jumping");
+            IsJumping = true;
             if (!JumpingSound.isPlaying)
             {
                 JumpingSound.Play();
@@ -108,10 +132,7 @@ public class FighterController : MonoBehaviour
             IsPunching = true;
             anim.SetTrigger("Punching");
             PunchingSound.Play();
-        }
-        else
-        {
-            IsPunching = false;
+            CanChangeHeight = false;
         }
 
         if (playerinput.PlayerMain.Kick.triggered && !IsKicking)
@@ -119,14 +140,56 @@ public class FighterController : MonoBehaviour
             IsKicking = true;
             anim.SetTrigger("Kicking");
             KickingSound.Play();
-        }
-        else
-        {
-            IsKicking = false;
+            CanChangeHeight = false;
         }
         
-        if(CoinCollected)
-            CoinCollected = false;
+    }
+
+    void Set()
+    {
+        IsJumping = false;
+        IsKicking = false;
+        IsPunching = false;
+        //IsMoving = false;
+        CanChangeHeight = true;
+    }
+
+    void SetJump()
+    {
+        IsKicking = false;
+        IsPunching = false;
+    }
+    void SetPunch()
+    {
+        IsKicking = false;
+        IsJumping = false;
+    }
+    void SetKick()
+    {
+        IsPunching = false;
+        IsJumping = false;
+    }
+    void UnSetJump()
+    {
+        IsJumping = false;
+        anim.SetTrigger("IdleTrig");
+        CanChangeHeight = true;
+    }
+    void UnSetPunch()
+    {
+        IsPunching = false;
+        anim.SetTrigger("IdleTrig");
+        CanChangeHeight = true;
+    }
+    void UnSetKick()
+    {
+        IsKicking = false;
+        anim.SetTrigger("IdleTrig");
+        CanChangeHeight = true;
+    }
+    void UnSetMoving()
+    {
+        IsMoving = false;
     }
 
     void AddVelocity()
@@ -136,17 +199,17 @@ public class FighterController : MonoBehaviour
 
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if(hit.gameObject.tag == "Coin" && !CoinCollected)
+        if(hit.gameObject.tag == "Coin" )
         {
-            ScoreManager scoreManager = GameObject.Find("Manager").GetComponent<ScoreManager>();
-            if(scoreManager.score < scoreManager.MaxCoins)
-            {
-                CoinCollected = true;
-                //Debug.Log("collision with coin");
-                hit.gameObject.SetActive(false);
-                scoreManager.IncreaseScore();
-                scoreManager.UpdateScore();
-            }
+            
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Hittable")
+        {
+            Debug.Log("Hit");
         }
     }
 
